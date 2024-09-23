ba8fca776a4d8a4683167d6c62804bfbbeb5db56
Salsabila <salsabilla.fadilla13@gmail.com>
2024-09-23 10:23:11 +0700

    Acara6

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,61 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  map!: L.Map;
 
   constructor() {}
 
+  ngOnInit() {}
+
+  ionViewDidEnter() {
+    this.map = L.map('mapId').setView([-7.768938753815898, 110.37778430008716], 12);
+
+    const osmLayer = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    });
+
+    const satelliteLayer = L.tileLayer('https://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}', {
+      subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
+      attribution: 'Map data &copy; <a href="https://google.com/maps">Google</a>',
+    });
+
+    const terrainLayer = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://opentopomap.org/copyright">OpenTopoMap</a>',
+    });
+
+    const cartoDBLayer = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
+      attribution: '&copy; <a href="https://carto.com/attributions">CARTO</a>',
+    });
+
+    osmLayer.addTo(this.map);
+
+    const baseMaps = {
+      'OpenStreetMap': osmLayer,
+      'Satellite': satelliteLayer,
+      'Terrain': terrainLayer,
+      'CartoDB Positron': cartoDBLayer,
+    };
+
+    L.control.layers(baseMaps).addTo(this.map);
+
+    const customIcon = L.icon({
+      iconUrl: 'https://static.vecteezy.com/system/resources/thumbnails/014/301/029/small_2x/red-pin-for-pointing-the-destination-on-the-map-3d-illustration-png.png',
+      iconSize: [25, 25],
+      iconAnchor: [12, 25],
+      popupAnchor: [0, -25],
+    });
+
+    const markersData = [
+      { coords: [-7.7698422, 110.3745493], popup: 'Universitas Gadjah Mada' },
+      { coords: [-7.785927, 110.370805], popup: 'Tugu Yogyakarta' },
+      { coords: [-7.752107, 110.377329], popup: 'Keraton Yogyakarta' },
+      { coords: [-7.782073, 110.363931], popup: 'Malioboro' },
+    ];
+
+    // Loop through the markers data to add markers
+    markersData.forEach(markerInfo => {
+      const marker = L.marker(markerInfo.coords as [number, number], { icon: customIcon }).addTo(this.map);
+      marker.bindPopup(markerInfo.popup);
+    });
+  }
 }
